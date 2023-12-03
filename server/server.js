435687ce5e985544e2463dff1b0ea868fad36f99
kocerhuseyin <huso1475@msn.com>
2023-12-03 22:13:47 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const mongoose = require('mongoose');
+
+// MongoDB URI
+const MONGO_URI = 'your_mongodb_uri';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
