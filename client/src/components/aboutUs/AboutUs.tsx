032a97197c01d1c2bf47a38a2f3aad6fd0a6f98c
İlkay Yüksel <ilkay.yuksel@gusto.com>
2023-12-11 17:09:15 +0300

    landing page created

---
+++ client/src/components/aboutUs/AboutUs.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-content-box display-6">
+      <h1 className="fw-bold text-center">Hakkımızda</h1>
+      <p className="mt-5">
+        Türkiye'de kaliteli zaman geçirmeyi amaçlayan bir platform olan Zaman
+        Dolusu'na hoş geldiniz. Misyonumuz, yetişkin bireylerin bağlantı
+        kurabileceği, öğrenebileceği ve eğlenebileceği canlı ve ilgi çekici bir
+        topluluk yaratmaktır. Zaman Dolusu'nda bireylerin refahı için sosyal
+        etkileşimin ve zihinsel uyarımın önemini anlıyoruz. Bu nedenle onların
+        ilgi alanlarına ve tercihlerine hitap eden çok çeşitli aktiviteler,
+        etkinlikler ve kaynaklar sunuyoruz. Platformumuz burada geçirilen her
+        anın anlamlı ve keyifli olmasını sağlıyor.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./Landing.css";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+import JoinUs from "../components/joinUs/JoinUs";
+import Discover from "../components/discover/Discover";
+import Works from "../components/works/Works";
+import AboutUs from "../components/aboutUs/AboutUs";
 
 const Landing: React.FC = () => {
-  return <h1>Landing</h1>;
+  return (
+    <>
+      <Navbar />
+      <div className="box">
+        <JoinUs />
+      </div>
+      <div className="box bg-light">
+        <Discover />
+      </div>
+      <div className="box bg-lighter">
+        <Works />
+      </div>
+      <div className="box bg-light">
+        <AboutUs />
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Landing;
