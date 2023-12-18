3264a7ee74e3ad8dc2e2fe9bee9d760df262c7e5
Mithat Ucar <mithat4226@hotmail.com>
2023-12-18 17:36:05 +0300

    Socket.io integration

--- server/server.js
+++ server/server.js
@@ -1,18 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // MongoDB URI
-const MONGO_URI = 'your_mongodb_uri';
+const MONGO_URI = "your_mongodb_uri";
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
